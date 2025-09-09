adda685fcd8ca829dfc6ad4af10e1ce052ee6482
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2025-09-09 08:52:07 +0300

    [Change] Button component: add variant prop for dynamic styling and update CSS module

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import css from './button.module.css'
 
 type Props = {
@@ -6,11 +5,17 @@ type Props = {
     onClick?: () => void;
     type?: "button" | "submit" | "reset" | undefined;
     disabled?: boolean;
+    variant?: "blue" | "green" | "red";
 }
 
-const Button = ({ title, onClick, type = "button", disabled }: Props) => {
+const Button = ({ title, onClick, type = "button", disabled, variant = "blue" }: Props) => {
     return (
-        <button className={css.button} onClick={onClick} type={type} disabled={disabled}>
+        <button
+            className={`${css.button} ${css[variant]}`}
+            onClick={onClick}
+            type={type}
+            disabled={disabled}
+        >
             {title}
         </button>
     )
